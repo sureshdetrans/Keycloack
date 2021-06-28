c5b853730b15e125c57139cc1bbb8337375e9d25
Suresh Talwar <suresh@detrans.io>
2021-06-28 20:32:18 +0530

    more fixese

--- src/Controller/RegisterationController.php
+++ src/Controller/RegisterationController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use App\Enum\KeyCloakConfig;
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class RegisterationController extends AbstractController
 {
@@ -30,11 +31,39 @@ class RegisterationController extends AbstractController
         $requestObj = json_decode($request->getContent());
         $authorization = $request->headers->get('Authorization');
 
-        var_dump($requestObj->enabled);
-        var_dump($authorization);
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/RegisterationController.php',
-        ]);
+        if ($authorization !== null && $requestObj !== null) {
+            try {
+                $data = json_encode(['enabled' => true, 'username' => $requestObj->username, 'emailVerified' => $requestObj->emailVerified,
+                    'email' => $requestObj->email, 'firstName' => $requestObj->firstName, 'lastName' => $requestObj->lastName]);
+
+                $response = $this->httpClient->request(
+                    "POST",
+                    KeyCloakConfig::Key_Cloak_Base_Url . KeyCloakConfig::Key_Cloak_Register_User,
+                    [
+                        'headers' => [
+                            'Authorization' => $authorization,
+                            'Content-Type' => "application/json",
+                        ],
+                        'body' => $data
+                    ]
+                );
+
+                if ($response && $response->getStatusCode() === 201) {
+                    $data = json_decode($response->getBody(), true);
+
+                    return $this->json($data, Response::HTTP_CREATED, array(
+                        'Content-Type' => 'application/json',
+                    ));
+                }
+
+            } catch (GuzzleException $e) {
+                return $this->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR, array(
+                    'Content-Type' => 'application/json',
+                ));
+            }
+        }
+
+        return $this->json("Error ", Response::HTTP_BAD_REQUEST);
+
     }
 }
--- src/Enum/KeyCloakConfig.php
+++ src/Enum/KeyCloakConfig.php
@@ -9,4 +9,5 @@ class KeyCloakConfig
     const Key_Cloak_Rest_Client = 'rest-client';
     const Key_Cloak_Client_Secret = '8cc68536-52a8-4cc1-a3fc-8448bed05b2d';
     const Key_Cloak_AuthToken_Url = "/auth/realms/master/protocol/openid-connect/token";
+    const Key_Cloak_Register_User = "/auth/admin/realms/master/users";
 }
